b2dfc594e442b1541bf98300e44e3c6fc0cdfe7f
gtw8g6krwv-lgtm <gtw8g6krwv@privaterelay.appleid.com>
2026-04-07 21:06:04 +1000

    Изменение названий переменных
    Переменные сделаны константами. Слово MESSAGE в названиях констант изменено на COMMAND

--- main.py
+++ main.py
@@ -4,29 +4,29 @@ from door import Door
 if __name__ == "__main__":
     door = Door()
 
-open_door_users_massege = "открыть"
-close_door_users_massege = "закрыть"
-check_door_users_massege = "проверить"
-enter_system_door_users_massege = "выход"
-goodbye_users_massege = "До свидания"
+OPEN_DOOR_USERS_COMMAND = "открыть"
+CLOSE_DOOR_USERS_COMMAND = "закрыть"
+CHECK_DOOR_USERS_COMMAND = "проверить"
+ENTER_SYSTEM_DOOR_USERS_COMMAND = "выход"
+GOODBYE_USERS_COMMAND = "До свидания"
 
 print("УПРАВЛЕНИЕ ДВЕРЬЮ")
-print(f"Доступные команды: {open_door_users_massege}, {close_door_users_massege}, {check_door_users_massege}, {enter_system_door_users_massege}")
+print(f"Доступные команды: {OPEN_DOOR_USERS_COMMAND}, {CLOSE_DOOR_USERS_COMMAND}, {CHECK_DOOR_USERS_COMMAND}, {ENTER_SYSTEM_DOOR_USERS_COMMAND}")
 
 while True:
     user_command = input("\nВведите команду: ").strip().lower()
 
-    if user_command == open_door_users_massege:
+    if user_command == OPEN_DOOR_USERS_COMMAND:
         door.open_door()
 
-    elif user_command == close_door_users_massege:
+    elif user_command == CLOSE_DOOR_USERS_COMMAND:
         door.close_door()
 
-    elif user_command == check_door_users_massege:
+    elif user_command == CHECK_DOOR_USERS_COMMAND:
         print(f"Дверь сейчас {door.get_door_state()}")
 
-    elif user_command == enter_system_door_users_massege:
-        print(goodbye_users_massege)
+    elif user_command == ENTER_SYSTEM_DOOR_USERS_COMMAND:
+        print(GOODBYE_USERS_COMMAND)
         break
     else:
-        print(f"Неизвестная команда! Используйте толкьо команды: {open_door_users_massege}, {close_door_users_massege}, {check_door_users_massege}, {enter_system_door_users_massege}")
+        print(f"Неизвестная команда! Используйте толкьо команды: {OPEN_DOOR_USERS_COMMAND}, {CLOSE_DOOR_USERS_COMMAND}, {CHECK_DOOR_USERS_COMMAND}, {ENTER_SYSTEM_DOOR_USERS_COMMAND}")
