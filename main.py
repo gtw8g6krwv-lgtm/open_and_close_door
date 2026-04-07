7f12dbcb79e59a8f8851cfe8b071236a5fb81b73
gtw8g6krwv-lgtm <gtw8g6krwv@privaterelay.appleid.com>
2026-04-07 19:36:32 +1000

    Update main.py
    Команды для управления дверью были вынесены в отдельные переменные

--- main.py
+++ main.py
@@ -4,23 +4,29 @@ from door import Door
 if __name__ == "__main__":
     door = Door()
 
+open_door_users_massege = "открыть"
+close_door_users_massege = "закрыть"
+check_door_users_massege = "проверить"
+enter_system_door_users_massege = "выход"
+goodbye_users_massege = "До свидания"
+
 print("УПРАВЛЕНИЕ ДВЕРЬЮ")
-print("Доступные команды: открыть, закрыть, проверить, выход")
+print(f"Доступные команды: {open_door_users_massege}, {close_door_users_massege}, {check_door_users_massege}, {enter_system_door_users_massege}")
 
 while True:
-    command = input("\nВведите команду: ").strip().lower()
+    user_command = input("\nВведите команду: ").strip().lower()
 
-    if command == "открыть":
+    if user_command == open_door_users_massege:
         door.open_door()
 
-    elif command == "закрыть":
+    elif user_command == close_door_users_massege:
         door.close_door()
 
-    elif command == "проверить":
+    elif user_command == check_door_users_massege:
         print(f"Дверь сейчас {door.get_door_state()}")
 
-    elif command == "выход":
-        print("До свидания")
+    elif user_command == enter_system_door_users_massege:
+        print(goodbye_users_massege)
         break
     else:
-        print("Неизвестная команда! Используйте толкьо команды: открыть, закрыть, проверить, выход")
+        print(f"Неизвестная команда! Используйте толкьо команды: {open_door_users_massege}, {close_door_users_massege}, {check_door_users_massege}, {enter_system_door_users_massege}")
