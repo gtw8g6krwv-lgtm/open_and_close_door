e537fbaa560851c79a66ad8c949ec4a53f66d290
gtw8g6krwv-lgtm <gtw8g6krwv@privaterelay.appleid.com>
2026-04-08 17:34:56 +1000

    Редакция названия переменных
    Убрано слово door в названиях переменных

--- door.py
+++ door.py
@@ -2,24 +2,24 @@ class Door:
     def __init__(self):
         self.__is_open = False
 
-    def open_door(self):
+    def open(self):
         if not self.__is_open:
             self.__is_open = True
             print("Дверь открыта!")
         else:
             print("Дверь уже открыта")
 
-    def close_door(self):
+    def close(self):
         if self.__is_open:
             self.__is_open = False
             print("Дверь закрыта")
         else:
             print("Дверь уже закрыта")
 
-    def check_open_door(self):
+    def test_open(self):
         return self.__is_open
 
-    def get_door_state(self):
+    def get_state(self):
         if self.__is_open:
             return "Открыта"
         else:
